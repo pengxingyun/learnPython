576c24149bb1a1dbfa4db3e861b13cf24c9f0220
pengxingyun <993630851@qq.com>
2017-10-09 01:07:30 +0800

    enum
    枚举类

--- basePython/customClass.py
+++ basePython/customClass.py
@@ -1,20 +1,32 @@
 # 定制类
 # 用类实现斐波那契数列
-class Fib(object):
-	def __init__(self):
-		self.a, self.b = 0, 1
-	def __iter__(self):
-		return self
-	def __getitem__(self, n):
-		a, b = 1, 1
-		for x in range(n):
-			a, b = b, a + b
-		return a
-	def __next__(self):
-		self.a, self.b = self.b, self.a + self.b
-		if self.a > 100000:
-			raise StopIteration()
-		return self.a # 返回下一个值
+# class Fib(object):
+# 	def __init__(self):
+# 		self.a, self.b = 0, 1
+# 	def __iter__(self):
+# 		return self
+# 	def __getitem__(self, n):
+# 		a, b = 1, 1
+# 		for x in range(n):
+# 			a, b = b, a + b
+# 		return a
+# 	def __next__(self):
+# 		self.a, self.b = self.b, self.a + self.b
+# 		if self.a > 100000:
+# 			raise StopIteration()
+# 		return self.a # 返回下一个值
 
-for n in Fib():
-	print(n)
+# for n in Fib():
+# 	print(n)
+	
+class Student(object):
+		def __init__(self, name):
+			self.name = name
+		# __str__定制返回类的描述字段
+		def __str__(self):
+			return 'Student object (name: %s)' % self.name
+
+		__repr = __str__
+
+print(Student('pengxingyun'))
+				
